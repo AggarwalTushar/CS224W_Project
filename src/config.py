2f49a45ec1cbe0596079620e273ca6f858fac9f9
AggarwalTushar <tushar.aggarwal53@gmail.com>
2025-11-29 21:54:49 -0800

    Updated Repo

--- src/config.py
+++ src/config.py
@@ -4,9 +4,9 @@ LOOKBACK_DAYS = 180  # 6 months of history
 PREDICTION_HORIZONS = [30, 90, 180]  # Predict 1, 3, 6 months ahead
 EPOCHS = 300
 LR = 2e-3
-WEIGHT_DECAY = 5e-4
+WEIGHT_DECAY = 1e-5
 HIDDEN_DIM = 256
 OUT_DIM = 256
 DROPOUT = 0.3
-BATCH_SIZE = 64  # Batch size for graphs (each graph = one time window)
+BATCH_SIZE = 16  # Batch size for graphs (each graph = one time window)
 OUT_DIR = "./model/"
